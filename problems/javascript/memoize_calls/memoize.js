37885ebf78663303ca44399d625539789e0e2651
Nour-eddineAE <nouraddine.ait.elhadj@gmail.com>
2023-08-20 16:36:39 +0100

    call memoize

---
+++ problems/javascript/memoize_calls/memoize.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const mem = {};
+    return function (...args) {
+        const key = JSON.stringify(args);
+        return key in mem ? mem[key] : (mem[key] = fn(...args));
+    };
+}
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
