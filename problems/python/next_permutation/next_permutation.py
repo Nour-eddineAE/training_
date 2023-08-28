c646eb79863cc2b3335b3aec7896dd90b2ebc36c
Nour-eddineAE <nouraddine.ait.elhadj@gmail.com>
2023-08-28 11:00:54 +0100

    next_permutation sol optimization

--- problems/python/next_permutation/next_permutation.py
+++ problems/python/next_permutation/next_permutation.py
@@ -8,20 +8,20 @@ class Solution:
         if list_len > 1:
             greatestToTheRight = list_len - 1
 
-            while greatestToTheRight >= 0:
-                if nums[greatestToTheRight - 1] < nums[greatestToTheRight]:
-                    break
+            while greatestToTheRight > 0 and nums[greatestToTheRight - 1] >= nums[greatestToTheRight]:
                 greatestToTheRight -= 1
 
-            to_swap_with = list_len - 1
-            while to_swap_with >= 0 and nums[to_swap_with] <= nums[greatestToTheRight - 1]:
-                to_swap_with -= 1
+            if greatestToTheRight == 0:
+                nums = nums.reverse()
+            else:
+                to_swap_with = list_len - 1
+                while to_swap_with >= 0 and nums[to_swap_with] <= nums[greatestToTheRight - 1]:
+                    to_swap_with -= 1
 
-            temp = nums[greatestToTheRight - 1]
-            nums[greatestToTheRight - 1] = nums[to_swap_with]
-            nums[to_swap_with] = temp
+                nums[greatestToTheRight -
+                     1], nums[to_swap_with] = nums[to_swap_with], nums[greatestToTheRight - 1]
 
-            nums[greatestToTheRight:] = sorted(nums[greatestToTheRight:])
+                nums[greatestToTheRight:] = reversed(nums[greatestToTheRight:])
 
             print(nums)
 
