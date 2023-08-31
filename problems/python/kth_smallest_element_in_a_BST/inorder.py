86e1e8f796306ec39fbcc8daa189375eda94d381
Nour-eddineAE <nouraddine.ait.elhadj@gmail.com>
2023-08-31 12:51:45 +0100

    kth smallest in a bst

---
+++ problems/python/kth_smallest_element_in_a_BST/inorder.py
@@ -0,0 +1,56 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left: TreeNode = left
+        self.right: TreeNode = right
+
+
+class Solution:
+    def inorder(self, root: TreeNode, res: List[int]) -> None:
+        if not root:
+            return
+
+        self.inorder(root.left, res)
+        res.append(root.val)
+        self.inorder(root.right, res)
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+            * This solution uses an inorder traversal of a binary search tree, which is sorted by def. 
+            * If the BST is modified often (i.e., we can do insert and delete operations)
+                and you need to find the kth smallest frequently, how would you optimize?
+
+                ? Storing Inorder Traversal Approach for Frequent kth Smallest Queries:
+
+                Benefits:
+
+                    1- Optimized Queries: Fetching the kth smallest element is constant time after constructing the list.
+                    2- Reduced Query Time: Fetch operation remains efficient regardless of tree size.
+                    3- Query Frequency Matters: Advantageous when there are multiple kth smallest queries after a batch of modifications.
+                    4- Amortized Efficiency: If query frequency is higher than modifications, overhead of list maintenance can be acceptable.
+                    5- Simplicity: Approach is simple to understand and implement.
+
+                Trade-offs:
+
+                    1- Memory Usage: Requires O(N) space to store traversal list.
+                    2- Modification Impact: Frequent insertions and deletions can impact efficiency due to list rebuilding.
+                    3- Balancing: Unbalanced tree due to modifications might reduce efficiency.
+                    4- Context Matters: Effectiveness depends on the balance between queries and modifications.
+        """
+        res = []
+        self.inorder(root, res)
+        return res[k-1]
+
+
+node6 = TreeNode(1)
+node5 = TreeNode(4)
+node4 = TreeNode(2, node6)
+node3 = TreeNode(6)
+node2 = TreeNode(3, node4, node5)
+root = TreeNode(5, node2, node3)
+
+
+print(Solution().kthSmallest(root, 1))  # 1
