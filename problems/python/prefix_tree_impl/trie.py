a90549187c37fda467eb4fc63bd540c1c60598d4
Nour-eddineAE <nouraddine.ait.elhadj@gmail.com>
2023-09-01 20:55:38 +0100

    implementation of a trie

---
+++ problems/python/prefix_tree_impl/trie.py
@@ -0,0 +1,47 @@
+"""
+    ? Basic operations on a trie
+"""
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+word = "apple"
+prefix = "app"
+obj = Trie()
+obj.insert(word)
+print(obj.search(word))  # True
+print(obj.startsWith(prefix))  # True
